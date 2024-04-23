5b2bb9c2efe4b94a367b1d8818a504331a38640f
User850413 <csyou1228@gmail.com>
2024-04-23 16:30:04 +0900

    feat : Router 태그로 페이지 나눔

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
+
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  );
 }
 
 export default App;
