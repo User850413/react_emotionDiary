5193248a58d0d8d951ee0c917ab976f8b1f2ba7f
User850413 <csyou1228@gmail.com>
2024-04-24 19:16:40 +0900

    feat : home 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,19 @@ import "./App.css";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-04-24").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-04-23").getTime(),
     emotionId: 3,
     content: "2번 일기 내용",
   },
   {
     id: 3,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-03-06").getTime(),
     emotionId: 2,
     content: "3번 일기 내용",
   },
@@ -48,8 +48,8 @@ function reducer(state, action) {
 }
 
 // context를 통해 아래로 공급
-const DiaryStateContext = createContext();
-const DiaryDispatchContent = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContent = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
